ad540551828ead93fb2ddf96c7d195eac8a3923b
Abdul Ghafoor <abdul.ghafoor@dubizzlelabs.com>
2024-12-20 23:26:58 +0500

    Update context docs

--- src/components/ReactContextAPI/ContextEX3MultiLevelContext.tsx
+++ src/components/ReactContextAPI/ContextEX3MultiLevelContext.tsx
@@ -8,21 +8,23 @@ const ContextAProvider = ({ children }: { children: ReactNode }) => {
   const ContexB = useContext(ContextB);
   console.log("ContexB value inside ContextAProvider: ", ContexB); // Undefined
   return <ContextA.Provider value="aaa">{children}</ContextA.Provider>;
-
-  // Reason:
-  // In React, context providers and consumers are used hierarchically:
-  // 1- A Provider makes a context value available to all its descendant components.
-  // 2- A Consumer (or useContext hook) can only access the value of a context from a provider higher in the tree
-  //  (in the React render hierarchy).
-  // ContextAProvider is wrapping ContextBProvider
-  // This means that ContextBProvider is a descendant of ContextAProvider
-  // When ContextAProvider is rendered, ContextBProvider has not been rendered yet, so the ContextB value is not
-  // available in the component tree above ContextAProvider
-  // In ContextAProvider, I called:
-  // const ContexB = useContext(ContextB);
-  // Since there is no ContextB.Provider rendered above ContextAProvider in the tree, the useContext(ContextB)
-  // returns undefined.
 };
+
+// In above code the result of
+// console.log("ContexB value inside ContextAProvider: ", ContexB);
+// will be undefined because
+// In React, context providers and consumers are used hierarchically:
+// 1- A Provider makes a context value available to all its descendant components.
+// 2- A Consumer (or useContext hook) can only access the value of a context from a provider higher in the tree
+//  (in the React render hierarchy).
+// ContextAProvider is wrapping ContextBProvider
+// This means that ContextBProvider is a descendant of ContextAProvider
+// When ContextAProvider is rendered, ContextBProvider has not been rendered yet, so the ContextB value is not
+// available in the component tree above ContextAProvider
+// In ContextAProvider, I called:
+// const ContexB = useContext(ContextB);
+// Since there is no ContextB.Provider rendered above ContextAProvider in the tree, the useContext(ContextB)
+// returns undefined.
 
 const ContextBProvider = ({ children }: { children: ReactNode }) => {
   const ContexA = useContext(ContextA);
